b033b21ab734547f857a5e61272aad83d8f9d0cc
whistler <wlr@tut.by>
2022-01-18 17:41:09 +0500

    Фолбэк классы для работы с инфблоками (для обратной совместимости)

---
+++ lib/Entity/IBlock/Entity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Itgro\Entity\IBlock;
+
+use Itgro\Entity\IBlock\Element\Entity as RefactoredEntity;
+
+/**
+ * Fallback for backward compatibility
+ *
+ * @deprecated
+ * @see \Itgro\Entity\IBlock\Element\Entity
+ */
+abstract class Entity extends RefactoredEntity
+{
+
+}
---
+++ lib/Entity/IBlock/IblockSection/Entity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Itgro\Entity\IBlockSection;
+
+use Itgro\Entity\IBlock\Section\Entity as RefactoredEntity;
+
+/**
+ * Fallback for backward compatibility
+ *
+ * @deprecated
+ * @see \Itgro\Entity\IBlock\Section\Entity
+ */
+abstract class Entity extends RefactoredEntity
+{
+
+}
